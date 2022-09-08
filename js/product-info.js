d4431ec22c588caaabdd68f153520a89cf91966a
elios11 <eliosferre3@hotmail.com>
2022-09-07 23:52:22 -0300

    Agregar comentarios de producto

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,28 @@
 let productID = localStorage.getItem("productID");
 let PRODUCT_URL = PRODUCT_INFO_URL + productID + EXT_TYPE;
+let PRODUCT_COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL + productID + EXT_TYPE;
 let productInfoArray = "";
+let productCommentsArray = "";
 const productTitle = document.getElementById("productTitle");
 
 //Devuelve un objeto utilizando un archivo JSON y luego ejecuta la función showProductInfo
 function getProdInfo() {
     getJSONData(PRODUCT_URL).then(function(resultObj) {
         productInfoArray = resultObj.data;
-        console.log(productInfoArray);
+    })
+}
+
+function getProdComments() {
+    getJSONData(PRODUCT_COMMENTS_URL).then(function(resultObj) {
+        productCommentsArray = resultObj.data;
         showProductInfo();
     })
 }
-document.addEventListener("DOMContentLoaded", getProdInfo());
+
+document.addEventListener("DOMContentLoaded", function() {
+    getProdInfo();
+    getProdComments();
+});
 
 function showProductInfo() {
     let htmlContentToAppend = `
@@ -61,23 +72,55 @@ function showProductInfo() {
             </div>
         </div>
         <div class="row pt-3">
-            <div>
-                <strong>
-                    Imágenes ilustrativas
-                </strong>
+            <strong>
+                Imágenes ilustrativas
+            </strong>
+            <div class="row pt-3 d-flex justify-content-center" id="imagesContainer">
+                ${getImages(productInfoArray)}
             </div>
         </div>
-        <div class="row pt-3 d-flex justify-content-center">
-            `
-            for(let i = 0; i < productInfoArray.images.length; i++) {
-                let currentImg = productInfoArray.images[i];
-                htmlContentToAppend += `
-                <img src="${currentImg}" alt="Imagen de ${productInfoArray.name}" class="img-thumbnail w-20 m-2">
-                `
-            }
-            `
+        <div class="row pt-5">
+            <h2 class="d-flex justify-content-center pb-4">
+                Opiniones
+            </h2>
+            <div class="container">
+            <hr>
+                ${getComments(productCommentsArray)}
+            </div>
         </div>
     </div>
     `
     document.getElementById("productInfoContainer").innerHTML = htmlContentToAppend;
+}
+
+function getImages(array) {
+    let imagesHTML = ``;
+    for(let i = 0; i < array.images.length; i++) {
+        let currentImg = array.images[i];
+        imagesHTML += `
+            <img src="${currentImg}" alt="Imagen de ${array.name}" class="img-thumbnail w-20 m-2">
+        `
+    }
+    return imagesHTML;
+}
+
+function getComments(array) {
+    let commentsHTML = ``;
+    for (let i = 0; i < array.length; i++) {
+        let currentComment = array[i];
+        commentsHTML += `
+        <div class="row m-2">
+                </div>
+                    <strong>${currentComment.user}</strong>
+                    <span class="text-muted">&nbsp- ${currentComment.dateTime}</span>
+                </div>
+            </div>
+            <div>
+                ${currentComment.description}
+            </div>
+            <hr class="mb-2 mt-3">
+        </div>
+        `
+    }
+    return commentsHTML;
 }
