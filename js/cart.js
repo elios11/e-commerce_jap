d97d7271489f18e5b337e476d1282a7798907b21
elios11 <eliosferre3@hotmail.com>
2022-10-17 21:49:46 -0300

    Agregar funcionalidad a costo de envío y productos

--- js/cart.js
+++ js/cart.js
@@ -1,19 +1,26 @@
 let userCartArray = [];
-const BROU_COTIZACION_API = "https://cotizaciones-brou.herokuapp.com/api/currency/latest";
-let CURRENT_USD_PRICE;
+let currentUSDPrice;
+let shippingTaxes;
+const BROU_COTIZATION_API = "https://cotizaciones-brou.herokuapp.com/api/currency/latest";
 const CART_CONTAINER = document.getElementById("cart");
+const PRODUCTS_COST_TEXT = document.getElementById("productsCost");
+const SHIPPING_COST_TEXT = document.getElementById("shippingCost");
+const TOTAL_COST_TEXT = document.getElementById("totalCost");
 
+// Guarda el precio actual del dólar en una variable
 async function getLatestCotizations(url) {
     const cotizationRes = await fetch(url);
     const cotizationData = await cotizationRes.json();
-    console.log(cotizationData);
+    currentUSDPrice = cotizationData.rates.USD.buy;
 }
 
 function getCartData() {
     updateFromLocalStorage(userCartArray);
     showUserCart(userCartArray);
-    removeItemFromCartBtn(userCartArray);
-    updateSubtotal(userCartArray);
+    removeItemFromCartBtn(userCartArray.articles);
+    updateSubtotal(userCartArray.articles);
+    getShippingTypeTax("shippingType");
+    showCostsValues();
 }
 
 // Verifica si el usuario inició sesión, y si lo hizo muestra el contenido de la página
@@ -27,7 +34,6 @@ document.addEventListener("DOMContentLoaded", () => {
         `
         return false;
     }
-    getLatestCotizations(BROU_COTIZACION_API);
     getCartData();
 })
 
@@ -36,7 +42,7 @@ function showUserCart(cartArray) {
     if (!cartArray.articles || cartArray.articles.length === 0) {
         document.getElementById("shippingDetails").innerHTML = "";
         CART_CONTAINER.innerHTML = `
-        <h2 class="text-center mt-5" role="alert">
+        <h2 class="text-center mt-5">
             El carrito de compras se encuentra vacío, mirá nuevos productos para agregar en
             <a href="categories.html" class="alert-link">categorías</a>.
         </h2>
@@ -55,7 +61,7 @@ function showUserCart(cartArray) {
                 <th scope="col">
                     Costo
                 </th>
-                <th scope="col">
+                <th class="w-8" scope="col">
                     Cantidad
                 </th>
                 <th scope="col">
@@ -111,6 +117,18 @@ function getArticles(cartArray) {
     return articlesHTMLContent;
 }
 
+// Muestra el subcosto de los productos, el costo de envío, y el costo total
+async function showCostsValues() {
+    let subcost = await getSubtotalValue();
+    let shippingCost = calculateShippingCost(subcost, shippingTaxes);
+    let totalCost = subcost + shippingCost;
+    PRODUCTS_COST_TEXT.innerHTML = `USD ${subcost.toLocaleString("EN")}`;
+    if (shippingTaxes) {
+        SHIPPING_COST_TEXT.innerHTML = `USD ${shippingCost.toLocaleString("EN")}`;
+        TOTAL_COST_TEXT.innerHTML = `USD ${totalCost.toLocaleString("EN")}`;
+    }
+}
+
 // Reemplaza los productos actuales del carrito por los del almacenamiento local
 function updateFromLocalStorage(array) {
     if (localStorage.getItem("storedCartProducts")) {
@@ -122,10 +140,10 @@ function updateFromLocalStorage(array) {
 }
 
 // Agrega evento input al input de cantidad de cada producto
-function updateSubtotal(objCartArray) {
+function updateSubtotal(objCartArticles) {
     const localStorageCartItems = JSON.parse(localStorage.getItem("storedCartProducts"));
 
-    objCartArray.articles.forEach(product => {
+    objCartArticles.forEach(product => {
         const productCountInput = document.getElementById(product.id);
         productCountInput.addEventListener("input", () => {
             localStorageCartItems.forEach(item => {
@@ -134,19 +152,16 @@ function updateSubtotal(objCartArray) {
                     localStorage.setItem("storedCartProducts", JSON.stringify(localStorageCartItems));
                 }
             })
-            updateFromLocalStorage(objCartArray);
-            showUserCart(objCartArray);
-            updateSubtotal(objCartArray);
-            removeItemFromCartBtn(objCartArray);
+            getCartData();
         })
     })
 }
 
 // Elimina producto elegido del carrito de compras y del almacenamiento local
-function removeItemFromCartBtn(objCartArray) {
+function removeItemFromCartBtn(objCartArticles) {
     const localStorageCartItems = JSON.parse(localStorage.getItem("storedCartProducts"));
 
-    objCartArray.articles.forEach(product => {
+    objCartArticles.forEach(product => {
         const removeItemBtn = document.getElementById(`rmvItem_${product.id}`);
         removeItemBtn.addEventListener("click", () => {
             localStorageCartItems.forEach(function (item, index) {
@@ -155,10 +170,55 @@ function removeItemFromCartBtn(objCartArray) {
                     localStorage.setItem("storedCartProducts", JSON.stringify(localStorageCartItems));
                 }
             })
-            updateFromLocalStorage(objCartArray);
-            showUserCart(objCartArray);
-            removeItemFromCartBtn(objCartArray);
-            updateSubtotal(objCartArray);
+            getCartData();
         })
     })
+}
+
+// Devuelve un array con todos los subtotales en dólares
+function extractSubtotals(objCartArticles) {
+    let subtotalsArray = [];
+    objCartArticles.forEach(element => {
+        if (element.currency === "UYU") {
+            subtotalsArray.push(Math.round((element.subtotal / currentUSDPrice)*100) / 100);
+        }
+        else {
+            subtotalsArray.push(element.subtotal);
+        }
+    });
+    return subtotalsArray;
+}
+
+// Devuelve la suma de todos los números de un array
+function sumOfSubtotals(arrayOfNumbers) {
+    let result = 0;
+    arrayOfNumbers.forEach(element => {
+        result += element;
+    });
+    return result;
+}
+
+// Devuelve el impuesto añadido por el tipo de envío elegido
+function getShippingTypeTax(inputName) {
+    let radioButtons = document.getElementsByName(inputName);
+
+    radioButtons.forEach(element => {
+        element.addEventListener("click", () => {
+            shippingTaxes = element.value;
+            console.log(shippingTaxes);
+            showCostsValues();
+        });
+    })
+}
+
+function calculateShippingCost(subcost, shippingTax) {
+    let shippingCost = 0;
+    shippingCost = Math.round((subcost * shippingTax / 100) * 100) / 100;
+    return shippingCost;
+}
+
+async function getSubtotalValue() {
+    await getLatestCotizations(BROU_COTIZATION_API);
+    let subtotal = sumOfSubtotals(extractSubtotals(userCartArray.articles));
+    return subtotal;
 }
