19b57a632f87467b026d4d36f37caa0adc3f6785
elios11 <eliosferre3@hotmail.com>
2022-10-27 23:05:23 -0300

    Modificar validaciones de inicio de sesión

--- js/login.js
+++ js/login.js
@@ -1,16 +1,41 @@
-let loginButton = document.getElementById("loginButton");
-let dataAlert = document.querySelector(".dataAlert");
+const LOGIN_BUTTON = document.getElementById("loginButton");
+const EMAIL_ALERT = document.getElementById("emailAlert");
+const PASSWORD_ALERT = document.getElementById("passwordAlert");
 
-loginButton.addEventListener("click", function() {
+LOGIN_BUTTON.addEventListener("click", function() {
     let email = document.getElementById("email");
     let password = document.getElementById("pass");
+    let isValidated = true; 
 
-    if (email.value !== "" && password.value !== "") {
-        localStorage.setItem("userEmail", email.value);
-        window.location = "portada.html";
+    if (!email.checkValidity()) {
+        isValidated = false;
+        email.classList.add("invalid-input");
+        EMAIL_ALERT.innerHTML = "Por favor ingrese un correo electrónico";
     }
     else {
-        dataAlert.innerHTML = "Por favor rellene todos los campos";
+        EMAIL_ALERT.innerHTML = "";
+        email.classList.remove("invalid-input");
+    }
+    
+    if (!password.checkValidity()) {
+        if (password.value.length > 0) {
+            PASSWORD_ALERT.innerHTML = "La contraseña es incorrecta";
+        }
+        else {
+            PASSWORD_ALERT.innerHTML = "Por favor ingrese su contraseña";
+        }
+        isValidated = false;
+        password.classList.add("invalid-input");
+    }
+    else {
+        PASSWORD_ALERT.innerHTML = "";
+        password.classList.remove("invalid-input");
+
+    }
+
+    if (isValidated) {
+        localStorage.setItem("userEmail", email.value);
+        window.location = "portada.html";
     }
 })
 
