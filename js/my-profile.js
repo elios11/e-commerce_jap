9ec7c192d5608c4ceec5cfc3861b3596bd9c5746
elios11 <eliosferre3@hotmail.com>
2022-11-04 01:14:49 -0300

    Agregar funcionalidades de perfil de usuario
    -Guarda y/u obtiene datos de usuario en almacenamiento local

---
+++ js/my-profile.js
@@ -0,0 +1,128 @@
+const FIRST_NAME = document.getElementById("first-name");
+const SECOND_NAME = document.getElementById("second-name");
+const FIRST_SURNAME = document.getElementById("first-surname");
+const SECOND_SURNAME = document.getElementById("second-surname");
+const EMAIL = document.getElementById("email");
+const PHONE_NUMBER = document.getElementById("phone-number");
+const IMAGE_INPUT = document.getElementById("uploadProfileImage");
+const PROFILE_IMAGE = document.getElementById("profile-image");
+const SAVE_CHANGES = document.getElementById("saveChangesBtn");
+const INVALID_FB_MSG = document.getElementById("invalidDataFB");
+const MODIFY_EMAIL_BTN = document.getElementById("modifyEmail");
+let LSProfileData = JSON.parse(localStorage.getItem("profile-data"));
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (!localStorage.getItem("userEmail")) {
+        document.querySelector("main").innerHTML = `
+        <div class="alert alert-warning lead text-center w-50 mt-5" role="alert">
+            Para ver su perfil, por favor
+            <a href="index.html" class="alert-link">inicie sesión</a>.
+        </div>
+        `
+        return false;
+    }
+    EMAIL.value = localStorage.getItem("userEmail");
+    fillDataFromLocalStorage();
+})
+
+//Permite modificar el correo
+MODIFY_EMAIL_BTN.addEventListener("click", () => {
+    EMAIL.toggleAttribute("disabled");
+    checkProfileInfoValidity();
+})
+
+//Transforma imagen elegida en el input a URL
+IMAGE_INPUT.addEventListener("change", () => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(IMAGE_INPUT.files[0]);
+
+    fileReader.addEventListener("load", () => {
+        const imageURL = fileReader.result;
+        PROFILE_IMAGE.src = imageURL;
+    })
+})
+
+//Verifica si los campos obligatorios fueron validados
+function checkProfileInfoValidity() {
+    let allValid = true;
+    if (!FIRST_NAME.checkValidity()) {
+        allValid = false;
+        FIRST_NAME.classList.add("invalid-input");
+        INVALID_FB_MSG.classList.add("d-block");
+    }
+    else {
+        FIRST_NAME.classList.remove("invalid-input");
+    }
+    if (!FIRST_SURNAME.checkValidity()) {
+        allValid = false;
+        FIRST_SURNAME.classList.add("invalid-input");
+        INVALID_FB_MSG.classList.add("d-block");
+    }
+    else {
+        FIRST_SURNAME.classList.remove("invalid-input");
+    }
+    if (!EMAIL.checkValidity()) {
+        allValid = false;
+        EMAIL.classList.add("invalid-input");
+        INVALID_FB_MSG.classList.add("d-block");
+    }
+    else {
+        EMAIL.classList.remove("invalid-input");
+    }
+    if (allValid) {
+        INVALID_FB_MSG.classList.remove("d-block");
+    }
+    return allValid;
+}
+
+//Chequea validación con evento input en los campos obligatorios
+function continuallyCheckValidity() {
+    const requiredInputs = [FIRST_NAME, FIRST_SURNAME, EMAIL];
+    requiredInputs.forEach(element => {
+        element.addEventListener("input", () => {
+            checkProfileInfoValidity();
+        })
+    })
+}
+
+//Guarda información de usuario en el localStorage
+SAVE_CHANGES.addEventListener("click", () => {
+    if (checkProfileInfoValidity()) {
+        if (!window.confirm("¿Estás seguro de que quieres cambiar los datos de tu perfil?")) {
+            return false;
+        }
+        let profileData = {};
+        if (LSProfileData) {
+            profileData = LSProfileData;
+        }
+        profileData = {
+            "first_name": FIRST_NAME.value,
+            "second_name": SECOND_NAME.value,
+            "second_surname": SECOND_SURNAME.value,
+            "first_surname": FIRST_SURNAME.value,
+            "second_surname": SECOND_SURNAME.value,
+            "user_email": EMAIL.value,
+            "phone_number": PHONE_NUMBER.value,
+            "profile_image": PROFILE_IMAGE.src
+        }
+        localStorage.setItem("userEmail", EMAIL.value);
+        localStorage.setItem("profile-data", JSON.stringify(profileData));
+        window.location = "my-profile.html";
+    }
+    else {
+        continuallyCheckValidity();
+    }
+})
+
+//Rellena campos de datos si ya fueron guardados anteriormente
+function fillDataFromLocalStorage() {
+    if (LSProfileData) {
+        FIRST_NAME.value = LSProfileData.first_name;
+        SECOND_NAME.value = LSProfileData.second_name;
+        FIRST_SURNAME.value = LSProfileData.first_surname;
+        SECOND_SURNAME.value = LSProfileData.second_surname;
+        EMAIL.value = LSProfileData.user_email;
+        PHONE_NUMBER.value = LSProfileData.phone_number;
+        PROFILE_IMAGE.src = LSProfileData.profile_image;
+    }
+}
